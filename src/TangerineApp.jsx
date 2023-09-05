cc5c8073fe0e6be7df3b2ce10269ec04c74cf6f6
Rasec Camacho <rasec.camacho19@gmail.com>
2023-09-04 21:30:07 -0700

    freature tailwind added

---
+++ src/TangerineApp.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+export const TangerineApp = () => {
+  return (
+    <h1 className='text-white text-3xl'>Tangerine School</h1>
+  )
+}
+
+
+export default TangerineApp
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import TangerineApp from './TangerineApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <TangerineApp />
+    </BrowserRouter>
   </React.StrictMode>,
 )
