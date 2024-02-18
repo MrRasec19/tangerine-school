e0dc4c4de0abf506cca2c5c60da5976af462b3e6
Rasec Camacho <rasec.camacho19@gmail.com>
2024-02-17 22:11:51 -0700

    feature: slider animation done

--- src/home/components/OpinionsSlider.jsx
+++ src/home/components/OpinionsSlider.jsx
@@ -2,17 +2,31 @@ import { opinons } from '../../data/opinions.js';
 import { OpinionCard } from './OpinionCard.jsx';
 
 export const OpinionsSlider = () => {
+
   return (
-    <div id="opinions-content" className='w-full'>
-        <div id="opinions-slide" className='w-full flex flex-nowrap overflow-hidden justify-center'>
-            {
-              opinons.map( (op) => {
-                return(
-                  <OpinionCard key={ op.id } opinion={ op.opinion } userName={ op.userName } userSocialName={ op.userSocialName } city={ op.city } state={ op.state } />
-                )
-              })
-            }
-        </div>
+    <div id="content" className='w-full'>
+      <div className='opinions-content'>
+        <div className='opinions-slide'>
+              {
+                opinons.map( (op) => {
+                  return(
+                    <OpinionCard key={ op.id } opinion={ op.opinion } userName={ op.userName } userSocialName={ op.userSocialName } city={ op.city } state={ op.state } />
+                  )
+                })
+              }
+          </div>
+      </div>
+      <div className='opinions-content'>
+        <div className='opinions-slide'>
+              {
+                opinons.map( (op) => {
+                  return(
+                    <OpinionCard key={ op.id } opinion={ op.opinion } userName={ op.userName } userSocialName={ op.userSocialName } city={ op.city } state={ op.state } />
+                  )
+                })
+              }
+          </div>
+      </div>
     </div>
   )
 }
