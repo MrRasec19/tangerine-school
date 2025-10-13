428ac67605e43d72c3aa1d7bd2cfa71ab681e812
Rasec Camacho <rasec.cc19@outlook.com>
2025-10-12 19:17:58 -0700

    fix: whyStudy style title

--- src/home/components/WhyStudyWhitUs.jsx
+++ src/home/components/WhyStudyWhitUs.jsx
@@ -7,7 +7,7 @@ export const WhyStudyWhitUs = () => {
       id="why-with-us"
       className="bg-orange-thiny px-10 py-16"
     >
-      <h2 className="text-orange text-3xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center mb-16">
+      <h2 className="text-orange text-3xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold text-center mb-16">
         ¿Por qué estudiar con nosotros?
       </h2>
 
